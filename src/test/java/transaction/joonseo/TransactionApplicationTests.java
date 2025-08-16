fd622f713dcfdba2231fa97efbdb7c9ae6e4d8cd
xunxxoie <ahh0520@naver.com>
2025-08-17 03:18:19 +0900

    feat: Initial commit

---
+++ src/test/java/transaction/joonseo/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package transaction.joonseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
